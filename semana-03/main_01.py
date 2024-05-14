5abe0f6ab9fef07ea3d74b172a3c516fa18c9825
Marcos de Brito <marcos.brito001@edu.pucrs.br>
2024-05-13 23:22:15 -0300

    S3-A01
    operadores relacionais

---
+++ semana-03/main_01.py
@@ -0,0 +1,25 @@
+# Expressões Lógicas - operadores lógicos
+
+# relacionais: >, <, =>, <=, !=, ==
+
+
+# num3 está no intervalo [0;10]
+# num3 > 0 and num3 <=10
+
+# num1 não está no intervalo [0;10]
+# num1 < 0 or num1 > 10
+
+# num2 é o maior dos 3 valores lidos
+# num2>num1 and num2>num3   Sempre devem ser combinados 2 a 2
+
+# num2 é divisível por 7
+# num2 % 7 == 0 
+
+# num2 e num3 são múltiplos
+# num2%num3 == 0 and num3%num2 == 0
+
+# Os três valores lidos são iguais
+# num1 == num2 and num1 == num3
+
+# Dois dos três valores são iguais
+# num1 == num2 or num1 == num3 or num2 == num3
